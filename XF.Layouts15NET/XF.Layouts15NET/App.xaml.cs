0064fd97c9c08ed7e2aabdfaf59aad0306f5eae5
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-02-19 21:47:46 -0300

    Aula de layouts

---
+++ XF.Layouts15NET/XF.Layouts15NET/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Layouts15NET.Views;
+
+namespace XF.Layouts15NET
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TabbedPageXF();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
