3f5335d021d019c3c6affa4775b8452e830ce9bf
Rogério Rodrigues <roger.rrodrigues@gmail.com>
2020-03-04 21:49:28 -0300

    list view

---
+++ XF.EssentialsFIAP/XF.EssentialsFIAP/XF.EssentialsFIAP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XF.EssentialsFIAP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lblInfo.Text = AppInfo.PackageName.ToString();
+            lblDevice.Text = DeviceInfo.Model.ToString();
+
+
+        }
+
+        private async void Ligar_Clicked(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOnAsync();
+        }
+
+        private async void Desligar_Clicked(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOffAsync();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://www.microsoft.com", BrowserLaunchMode.SystemPreferred);
+
+        }
+    }
+}
---
+++ XF.ListView/XF.ListView/XF.ListView/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ListView
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+            lstEstados.ItemsSource = listaContatinho;
+        }
+
+        public List<Contatinhos> listaContatinho = new List<Contatinhos> {
+                new Contatinhos { Nome = "Juliana", Bairro = "Itaquera", Origem = "Happen" },
+                new Contatinhos { Nome = "Mariana", Bairro = "Capão Redondo", Origem = "Tinder"},
+                new Contatinhos { Nome = "Simone", Bairro = "Tatuape", Origem ="POF"},
+                new Contatinhos { Nome = "Maria", Bairro = "Itaquera", Origem = "Happen" },
+                new Contatinhos { Nome = "Carol", Bairro = "Capão Redondo", Origem = "Tinder"},
+                new Contatinhos { Nome = "Patricia", Bairro = "Tatuape", Origem ="POF"},
+                new Contatinhos { Nome = "Juliana M.", Bairro = "Itaquera", Origem = "Happen" },
+                new Contatinhos { Nome = "Vivian", Bairro = "Capão Redondo", Origem = "Tinder"},
+                new Contatinhos { Nome = "Roberta A", Bairro = "Itaquera", Origem = "Happen" },
+                new Contatinhos { Nome = "Amanda", Bairro = "Capão Redondo", Origem = "Tinder"},
+                new Contatinhos { Nome = "Cleonice", Bairro = "Tatuape", Origem ="POF"},
+                new Contatinhos { Nome = "Jurema", Bairro = "Tatuape", Origem ="POF"},
+                };
+
+        public class Contatinhos
+        {
+            public string Nome { get; set; }
+            public string Bairro { get; set; }
+            public string Origem { get; set; }
+        }
+
+        List<String> estados = new List<String>
+        {
+            "Acre (AC)","Alagoas (AL)","Amapá (AP)","Amazonas (AM)","Bahia (BA)","Ceará (CE)","Distrito Federal (DF)","Espírito Santo (ES)",
+            "Goiás (GO)","Maranhão (MA)","Mato Grosso (MT)","Mato Grosso do Sul(MS)","Minas Gerais(MG)","Pará(PA)","Paraíba(PB)",
+            "Paraná(PR)","Pernambuco(PE)","Piauí(PI)","Rio de Janeiro(RJ)","Rio Grande do Norte(RN)","Rio Grande do Sul(RS)",
+            "Rondônia(RO)","Roraima(RR)","Santa Catarina(SC)","São Paulo(SP)","Sergipe(SE)","Tocantins(TO)"
+        };
+
+        private void searchestados_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            var texto = searchestados.Text;
+
+            lstEstados.ItemsSource = listaContatinho.Where(x => x.Bairro.ToLower().Contains(texto.ToLower()));
+
+        }
+    }
+}
